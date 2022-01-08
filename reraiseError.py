a5296397617028e4ac4840bd8d67047de780713e
unknown <asahu.atiwa@gmail.com>
2022-01-09 00:05:03 +0530

    Imporved reraiseError.py

--- reraiseError.py
+++ reraiseError.py
@@ -8,7 +8,8 @@ def convert(s):
         return int(s)
     # allow us to gracefully handle an exception.
     except (ValueError, TypeError) as e:
-        print(f"Conversion Error: {str(e)}", file=sys.stderr)
+        # stderr outputs the message in form of an error.
+        print(f"Conversion Error: {str(e)}",file=sys.stderr)
         # allow us to re-raise the error currently being handled.
         raise
 if __name__ == "__main__":
