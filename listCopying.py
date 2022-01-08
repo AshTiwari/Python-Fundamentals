df9a80ce02c8b71ffa43fe80b30523f903776dd2
unknown <asahu.atiwa@gmail.com>
2022-01-08 21:22:49 +0530

    Python scripts

--- listCopying.py
+++ listCopying.py
@@ -1,4 +1,5 @@
 # list copying
+from copy import deepcopy
 
 def equaltoOperator():
     a = [1,2,3,4,5]
@@ -7,24 +8,27 @@ def equaltoOperator():
     print(a == b)   # => True
 
 def listSlicing():
-    a = [1,2,3,4,5]
+    a = [1,[2],3,4,5]
     b = a[:]
     a.append(6)
     print(a == b)   # => False
+    print(a[1] == b[1])     # => True
 
 def copyMethod():
-    a = [1,2,3,4,5]
+    a = [1,[2],3,4,5]
     b = a.copy()
     a.append(6)
     print(a == b)   # => False
+    print(a[1] == b[1])     # => True
 
 def listComprehension():
-    a = [1,2,3,4,5]
+    a = [1,[2],3,4,5]
     b = [item for item in a]
     a.append(6)
     print(a == b)   # => False  
+    print(a[1] == b[1])     # => True
 
-def shallowCopying():
+def copyFunction():
     a = [[1,2],[2,3]]
     b = a.copy()
     # It shows that a and b refer to different lists
@@ -32,13 +36,16 @@ def shallowCopying():
     print(a is b)   # => False
     print(a[0] is b[0]) # => True
     
+def deepcopyFunc():
+    a = [1,2,[3]]
+    b = deepcopy(a)
+    print(a[2] == b[2])     # False
     
 if __name__ == "__main__":
     # The new list will refer to the same list object.
     equaltoOperator()
-    # The new list will refer to a new list Object. 
+    # The new list will refer to a new list Object but list within them will refer to same obj. 
     listSlicing()
     copyMethod()
-    listComprehension()
-    # Shallow Copying
-    shallowCopying()
+    copyFunction()
+    deepcopyFunc()
